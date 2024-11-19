1feacf975996cf447a335bba0771dae4d3a72b21
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-11-19 15:59:31 +0900

    [fix] 5번 회전값 저장
    회전값 저장 시도 중 시간오버

--- Q_05/Assets/Scripts/GameManager.cs
+++ Q_05/Assets/Scripts/GameManager.cs
@@ -7,10 +7,16 @@ public class GameManager : SingletonBehaviour<GameManager>
 {
     public float Score { get; set; }
 
+    // 씬 시작마다 초기화 됨
     private void Awake()
     {
         SingletonInit();
-        Score = 0.1f;
+        // 스코어가 처음 씬이동시에만 0.1로 지정되게 해야됨
+        if (Score == 0)
+        {
+            Score = 0.1f;
+        }
+        // 흠....
     }
 
     public void Pause()
