cb658009523032b63f7c00fe5b17c7a674ee8c5e
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-11-19 11:22:41 +0900

    [fix] 1번 문제 오류 수정
    오브젝트 생성도 전에 참조하는 문제
    값을 넣어준 _cubeSetPoint를 SetPosition에 전달하지 않아 수치가 적용이 안된 문제
    수정 완료

--- Q_01/Assets/Scripts/CubeController.cs
+++ Q_01/Assets/Scripts/CubeController.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
     public Vector3 SetPoint { get; private set; }
 
-    public void SetPosition()
+    // 값을 받아올수있게 Vector3 SetPoint 추가
+    public void SetPosition(Vector3 SetPoint)
     {
-        transform.position = SetPoint;
+        // 게임오브젝트 추가
+        Debug.Log($"{SetPoint}");
+        gameObject.transform.position = SetPoint;
     }
 }
--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -4,33 +4,37 @@ using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
+    // 큐브 프리펩
     [SerializeField] private GameObject _cubePrefab;
-
+    // 큐브 컨트롤러 참조
     private CubeController _cubeController;
+    // 큐브 좌표
     private Vector3 _cubeSetPoint;
 
     private void Awake()
     {
-        SetCubePosition(3, 0, 3);
+        // 큐브 생성
+        CreateCube();       
     }
 
     private void Start()
     {
-        CreateCube();
+        SetCubePosition(3, 0, 3);
     }
 
+    // 생성이 안되었는데 큐브 컨트롤러를 참조하여 오류
     private void SetCubePosition(float x, float y, float z)
     {
         _cubeSetPoint.x = x;
         _cubeSetPoint.y = y;
         _cubeSetPoint.z = z;
-        _cubeController.SetPosition();
+        _cubeController.SetPosition(_cubeSetPoint);
     }
 
     private void CreateCube()
     {
         GameObject cube = Instantiate(_cubePrefab);
         _cubeController = cube.GetComponent<CubeController>();
-        _cubeSetPoint = _cubeController.SetPoint;
+        _cubeSetPoint = _cubeController.SetPoint; // 0,0,0
     }
 }
